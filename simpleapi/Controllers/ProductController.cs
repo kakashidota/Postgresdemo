d455aa2737152e5da928a960ffa0c3d8c734ddf3
Robin Kamo <robin.kamo@iths.se>
2023-06-01 15:19:18 +0200

    Add project files.

---
+++ simpleapi/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using simpleapi.Data;
+using simpleapi.Models;
+
+namespace simpleapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly ApiDbContext _context;
+
+        public ProductController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetAllProducts")]
+        public async Task<IActionResult> Get()
+        {
+            
+            var product = new Product()
+            {
+                Name = "King Edward potatoes",
+                ProductId = 44
+            };
+
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+
+            var allproducts = await _context.Products.ToListAsync();
+
+            return Ok(allproducts);
+
+        }
+    }
+}
---
+++ simpleapi/Data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using simpleapi.Models;
+
+namespace simpleapi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
